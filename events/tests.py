b955114015834eec122eb2b9077aed8b61bbec7c
Samuel Cury <cury.samuel@gmail.com>
2018-12-04 18:26:14 -0200

    configure and first test

--- events/tests.py
+++ events/tests.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
+from django.urls import reverse
+from events import models
+from django.test import Client
+# import json
 
-# Create your tests here.
+
+class EventTest(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user('joe', 'joe@joe.com', 'sam12345')
+        self.evento = models.Event.objects.create(name='Rock in Rio', participantes=80000, author=self.user)
+
+    def test_list_view(self):
+        self.client.login(username='joe', password='sam12345')
+        response = self.client.get(reverse('event_list'))
+        self.assertEqual(response.status_code, 200)
+        # response = self.client.post(reverse(check_item), json.dumps({}), content_type='application/json')
+        
